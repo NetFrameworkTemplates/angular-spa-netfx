6cb6fb264ffebe7296a4d412b259f679bcb48125
Demis Bellot <demis.bellot@gmail.com>
2019-08-10 23:07:27 -0400

    Update

--- MyApp/Configure.Ui.cs
+++ MyApp/Configure.Ui.cs
@@ -8,7 +8,7 @@ namespace MyApp
         public void Configure(IAppHost appHost)
         {
             // if wwwroot/ is empty, build Client App with 'npm run build'
-            var svgDir = appHost.RootDirectory.GetDirectory("/svg"); 
+            var svgDir = appHost.RootDirectory.GetDirectory("/assets/svg"); 
             if (svgDir != null)
             {
                 Svg.Load(svgDir);
